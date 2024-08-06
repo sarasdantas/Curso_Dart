8d5d9cfb1ab4a5f26f73ba346c38469faa711aba
Sara S. Dantas <160888218+sarasdantas@users.noreply.github.com>
2024-08-06 17:44:41 -0300

    map reduce

---
+++ funções/map_reduce_2.dart
@@ -0,0 +1,20 @@
+main() {
+  var notas = [7.3, 5.4, 7.7, 8.1, 5.5, 4.9, 9.1, 10.0];
+  var total = notas.reduce(somar);
+  print(total);
+
+  // FORMA COM O FOR IN
+  // var total = 0.0;
+  //   como notas é double, o total tbm tem que ser
+  
+  // for(var nota in notas) {
+  //   total += nota;
+  // }
+
+  // print(total);
+}
+
+double somar(double a, double b) {
+  print("$a $b"); // Aqui vai imprimir a conta 'detalhada', passo a passo
+  return a + b;
+}
