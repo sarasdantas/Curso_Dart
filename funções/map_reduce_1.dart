30a1c107c9a992ebcf34bfa53a9c6876ae305ce1
Sara S. Dantas <160888218+sarasdantas@users.noreply.github.com>
2025-02-22 11:02:09 -0300

    erro dart

--- funções/map_reduce_1.dart
+++ funções/map_reduce_1.dart
@@ -11,19 +11,22 @@ main() {
 
   String Function(Map) pegarApenasONome = (aluno) => aluno['nome'];
   // Function que irá retornar apenas o valor das chaves nome
+  var nomes = alunos.map(pegarApenasONome);
+  print(nomes);
 
-  int Function(String) qtdeDeLetras = (texto) => texto.length;
-  // Function que irá retornar valores do tipo int com a qtde de letras
 
-  int Function(int) dobro = (numero) => numero * 2;
-  // Vai multiplicar por 2 
+  // int Function(String) qtdeDeLetras = (texto) => texto.length;
+  // // Function que irá retornar valores do tipo int com a qtde de letras
 
-  var quantidadesDeLetras = alunos
-    .map(pegarApenasONome)
-    .map(qtdeDeLetras)
-    .map(dobro);
-  // Var que recebe as funções definidas acima
-  // Tem que colocá-los em ordem para aplicar na variável desejada
+  // int Function(int) dobro = (numero) => numero * 2;
+  // // Vai multiplicar por 2 
 
-  print(quantidadesDeLetras); // Imprimir resultado no console
+  // var quantidadesDeLetras = alunos
+  //   .map(pegarApenasONome)
+  //   .map(qtdeDeLetras)
+  //   .map(dobro);
+  // // Var que recebe as funções definidas acima
+  // // Tem que colocá-los em ordem para aplicar na variável desejada
+
+  // print(quantidadesDeLetras); // Imprimir resultado no console
 }
